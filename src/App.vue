3bab98e42dfaf45dbe69ad91ac2bf72ee0d09d81
rethes <rethesmutua@gmail.com>
2019-09-03 15:33:57 +0300

    feat(app): update landing page
    - update landing page
    - update archivedPosts
    
    [Finishes #168261974]

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,6 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/categories">Categories</router-link> |
       <router-link to="/posts">News Articles</router-link> |
       <router-link to="/trash">Trash</router-link>
     </div>
@@ -12,21 +10,5 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+  @import "../src/assets/scss/components/app.scss";
 </style>
--- src/components/ArchivedPosts/archivedPosts.vue
+++ src/components/ArchivedPosts/archivedPosts.vue
@@ -1,41 +1,38 @@
 <template>
   <div class="archived-list">
     <div class="header-row">
-      <h1 class="h1-heading"> Trash ({{bin.length}}) </h1>
+      <h1 class="h1-heading"> Trash List ({{bin.length}}) </h1>
     </div>
-    <div v-if="bin.length > 0" class="table">
-      <div class="table-header">
-        <div class="table-values">
-          <h3 class="h3-headings">Trashed Article</h3>
-        </div>
-        <div class="table-values">
-          <h3 class="h3-headings">Actions</h3>
-        </div>
-      </div>
-      <div class="table-rows" v-for="value in bin" :key="value.id">
-        <div class="table-value-right">
+    <ul v-if="bin.length > 0">
+      <li v-for="value in bin" :key="value.id" >
+        <div class="posts-list-content-items">
+          <div class="create-img">
+            <img src="../../assets/images/Album-create.svg"/>
+          </div>
           <div class="posts-list-content-values">
             <div class="posts-list-content-header">
               {{ value.title }}
             </div>
-            <div v-if=" value.description.length < 400">
-              {{ value.description }}
+            <div class="posts-list-content-description">
+              <div v-if=" value.description.length < 500">
+                {{ value.description }}
+              </div>
+              <div v-else>
+                {{ value.description.substring(0, 499) + '...' }}
+              </div>
             </div>
-            <div v-else>
-              {{ value.description.substring(0, 399) + '...' }}
+            <div class="action-buttons" style="padding: 10px;">
+              <button class="action-buttons__archive" @click="recover(value)">
+                Reset
+              </button>
+              <button class="action-buttons__delete" @click="remove(value)">
+                Delete
+              </button>
             </div>
           </div>
         </div>
-        <div class="table-values action-buttons">
-          <button class="action-buttons__archive" @click="recover(value)">
-            Reset
-          </button>
-          <button class="action-buttons__delete" @click="remove(value)">
-            Delete
-          </button>
-        </div>
-      </div>
-    </div>
+      </li>
+    </ul>
     <div v-else>
       <h3 class="h3-heading"> No Articles Found </h3>
     </div>
@@ -82,6 +79,5 @@ export default {
   @import "../../assets/scss/components/common/action-buttons";
   @import "../../assets/scss/components/common/global.scss";
   @import "../../assets/scss/components/view-post.scss";
-  @import "../../assets/scss/components/common/table.scss";
   @import "../../assets/scss/components/archived-posts.scss";
 </style>
